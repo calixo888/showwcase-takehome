752249302e7b363bc2cb55e3ffbe26c5b35fd035
Calix Huang <calix.huang1@gmail.com>
2020-09-15 20:41:52 -0700

    setup react modal display, trigger, and styling

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,10 +4,31 @@ import {
   addName,
   getName,
 } from '../reducers/User.js';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 function Dashboard() {
   const name = useSelector(getName);
   const dispatch = useDispatch();
+  const [educationModalShow, setEducationModalShow] = useState(false);
+
+  const showEducationModal = () => {
+    setEducationModalShow(true);
+  }
+
+  const hideEducationModal = () => {
+    setEducationModalShow(false);
+  }
 
   // REDIRECT TO HOME IF NAME DOES NOT EXIST
   useEffect(() => {
@@ -19,6 +40,12 @@ function Dashboard() {
   return (
     <div className="center-container">
       <h1>{name}'s Dashboard</h1>
+
+      <button type="button" onClick={showEducationModal}>Add Education</button>
+
+      <Modal isOpen={educationModalShow} onRequestClose={hideEducationModal} style={customStyles}>
+        <h3>Add Education</h3>
+      </Modal>
     </div>
   )
 }
